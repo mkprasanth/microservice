86b72f0f5e398727cfbe40e323ce82ba7c880958
prmk <prasanth.mk@amdocs.com>
2019-04-11 20:26:55 -0500

    First Commit

---
+++ currency-exchange-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1001,'USD','IND',70,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1002,'CAD','IND',60,0);
+insert into exchange_value(id,currency_from,currency_to,conversion_multiple,port)
+values(1003,'AED','IND',18,0);
